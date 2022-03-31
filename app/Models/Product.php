94206cb06e9ffc3fba30a2306ba970e0a81ab893
Haleem Hosein <haleemhosein@yahoo.com>
2022-03-30 21:43:33 -0400

    13 Adding product images with MediaLibrary

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     public function formattedPrice()
     {
@@ -17,5 +22,11 @@ class Product extends Model
     public function variations()
     {
         return $this->hasMany(Variation::class);
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
     }
 }
