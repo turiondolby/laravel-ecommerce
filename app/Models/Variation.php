fc8633ad0933772ac55b1a13fd642a6a67f5e88d
Haleem Hosein <haleemhosein@yahoo.com>
2022-04-01 19:58:56 -0400

    16 Adding media to product variations

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
-class Variation extends Model
+class Variation extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
     use HasRecursiveRelationships;
 
      public function formattedPrice()
@@ -46,5 +51,17 @@ class Variation extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb200x200')
+            ->fit(Manipulations::FIT_CROP, 200, 200);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('default')
+            ->useFallbackUrl(url('/storage/no-product-image.png'));
     }
 }
