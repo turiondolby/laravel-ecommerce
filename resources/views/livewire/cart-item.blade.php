0fe1594e66770cc0644924e4b197dcafba458810
Haleem Hosein <haleemhosein@yahoo.com>
2022-04-10 13:04:20 -0400

    25 Outputting cart items

--- resources/views/livewire/cart-item.blade.php
+++ resources/views/livewire/cart-item.blade.php
@@ -1,3 +1,35 @@
-<div>
-    {{ $variation->title }}
+<div class="border-b py-3 flex items-start last:border-0 last:pb-0">
+    <div class="w-20 mr-4">
+        <img src="{{ $variation->getFirstMediaUrl('default', 'thumb200x200') }}" class="w-20">
+    </div>
+
+    <div class="space-y-2">
+        <div>
+            <div class="font-semibold text-lg">
+                {{ $variation->formattedPrice() }}
+            </div>
+            <div class="space-y-1">
+                <div>{{ $variation->product->title }}</div>
+
+                <div class="flex items-center text-sm">
+                    Variation specifics
+                </div>
+            </div>
+        </div>
+
+        <div class="flex items-center space-x-4">
+            <div class="text-sm flex items-center space-x-2">
+                <div class="font-semibold">Quantity</div>
+                <select class="text-sm border-none">
+                    @for($quantity = 1; $quantity <= $variation->stockCount(); $quantity++)
+                        <option value="{{ $quantity }}">{{ $quantity }}</option>
+                    @endfor
+                </select>
+            </div>
+
+            <button class="text-sm">
+                Remove
+            </button>
+        </div>
+    </div>
 </div>
